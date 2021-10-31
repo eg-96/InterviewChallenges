5ffc3643badb1425139bace9b3918d2f8780247f
Enmanuel García <egarciag@ucenfotec.ac.cr>
2021-10-31 17:08:35 -0600

    Added Palindrome unit test

---
+++ InterviewChallenges/UnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace InterviewChallenges
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void ValidatePalindromeWithLinQ()
+        {
+            var word = "Ana".ToLower();
+            var isPalindrome = word.SequenceEqual(word.Reverse());
+
+            Assert.True(isPalindrome);
+        }
+
+        [Fact]
+        public void ValidatePalindrome()
+        {
+            var word = "Ana".ToLower();
+            var firstHalf = word.Substring(0, word.Length / 2);
+            var wordArray = word.ToCharArray();
+
+            Array.Reverse(wordArray);
+
+            var tempWord = new string(wordArray);
+            var secondHalf = tempWord.Substring(0, tempWord.Length / 2);
+
+            var isPalindrome = firstHalf.Equals(secondHalf);
+
+            Assert.True(isPalindrome);
+        }
+    }
+}
